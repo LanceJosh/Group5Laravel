c1865aeb161fcde53dd563c30a099323da3528ae
LanceJosh <lancejoshb@gmail.com>
2023-11-09 23:37:26 +0800

    updates

--- Group5/resources/views/dashboard.blade.php
+++ Group5/resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
     </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Users') }}
 
 
 
