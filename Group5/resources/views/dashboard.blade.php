6a5533333540330feec8f422a04b99d412fb1fb1
charm2602 <118341084+charm2602@users.noreply.github.com>
2023-11-11 22:28:46 +0800

    Landing Pages

--- Group5/resources/views/dashboard.blade.php
+++ Group5/resources/views/dashboard.blade.php
@@ -1,73 +1,15 @@
 <x-app-layout>
-    <style>
-        footer {
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            background-color: #0099ff;
-            color: white;
-            text-align: center;
-            padding: 10px 0;
-        }
-
-        .footer-content {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 0 20px;
-        }
-    </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Users') }}
-
-
-
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-            <div class="container">
-                <div class="row">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Created At</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            @php
-                            $i = 1;
-                            @endphp
-
-
-                            @foreach($users as $user)
-                            <tr>
-                                <th scope="row">{{$i++}}</th>
-                                <td>{{$user->name}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$user->created_at}}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
             </div>
-
-
-
-
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,96L48,133.3C96,171,192,245,288,245.3C384,245,480,171,576,160C672,149,768,203,864,229.3C960,256,1056,256,1152,240C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-        <footer>
-            <div class="footer-content">
-                <p>© 2023 HireLab&Co. | All rights reserved.</p>
-            </div>
-        </footer>
+    </div>
 </x-app-layout>
