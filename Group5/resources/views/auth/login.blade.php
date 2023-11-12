f5bdb850068627b615b79c2f5358ce59368c19bd
charm2602 <118341084+charm2602@users.noreply.github.com>
2023-11-12 16:43:58 +0800

    login

--- Group5/resources/views/auth/login.blade.php
+++ Group5/resources/views/auth/login.blade.php
@@ -1,7 +1,37 @@
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+ <title>Hire Lab&Co. | Login</title>
+ <meta name="description" content="Online Job Management / Job Portal" />
+	<meta name="keywords" content="job, work, resume, applicants, application, employee, employer, hire, hiring, human resource management, hr, online job management, company, worker, career, recruiting, recruitment" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="Hire Lab&Co." />
+    <meta property="og:description" content="Online Job Management / Job Portal" />
+    <link rel="shortcut icon" href="images/logo.png">
+
+	
+	<link rel="stylesheet" href="icons/linearicons/style.css">
+	<link rel="stylesheet" href="icons/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="icons/simple-line-icons/css/simple-line-icons.css">
+	<link rel="stylesheet" href="icons/ionicons/css/ionicons.css">
+	<link rel="stylesheet" href="icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="icons/rivolicons/style.css">
+	<link rel="stylesheet" href="icons/flaticon-line-icon-set/flaticon-line-icon-set.css">
+	<link rel="stylesheet" href="icons/flaticon-streamline-outline/flaticon-streamline-outline.css">
+	<link rel="stylesheet" href="icons/flaticon-thick-icons/flaticon-thick.css">
+	<link rel="stylesheet" href="icons/flaticon-ventures/flaticon-ventures.css">
+<link href="css/Login.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+        <img src="/images/logo.png" alt="Logo" class="logo" width = "100" height = "100">
         </x-slot>
 
         <x-validation-errors class="mb-4" />
@@ -14,35 +44,43 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
+            <br>
+           <center> <h2 id = "mainLabel"> Login </h2> </center>
+            <br>
+ <div class="flex items-center">
+    <i class="fa fa-envelope icon"></i>
+    <x-label for="email" value="{{ __('Email') }}" class="ml-2"></x-label></div>
+    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+<br>
+<div class="mt-4 flex items-center">
+    <i class="fa fa-lock icon"></i>
+    <x-label for="password" value="{{ __('Password') }}" class="ml-2"/></div>
+    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password"/>
+    <div class="flex items-center justify-between">
+    <div class="block mt-4">
+        <label for="remember_me" class="flex items-center">
+            <x-checkbox id="remember_me" name="remember" />
+            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+        </label>
+    </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+    <div class="mt-4">
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
+        @endif
+    </div>
+</div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+ 
+           <br><br>     
+           <center><div class="mt-4 mx-auto text-center"> <!-- Add mx-auto and text-center for horizontal and text centering -->
+        <x-button>
+            {{ __('Log in') }}
+        </x-button></center>
+    </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
@@ -83,9 +121,10 @@ margin-top: 70px;
 #mainLabel {
     font-size: 30px;
     color: #004d99;
-    letter-spacing: 3px;
+    letter-spacing: 2px;
     line-height: 0.2em;
-    font-weight: bold;
+    font-weight: bolder;
+    font-style: sans-serif;
 
 }
 
@@ -161,7 +200,7 @@ i {
     
 }
 
-#loginButton {
+.ml-4 {
     padding-left: 2.5rem;
     padding-right: 2.5rem;
     background-color: #001a33;
@@ -170,9 +209,11 @@ i {
     border-radius: 20px;
     color:aliceblue;
     border: 1px solid #001a33;
+    text-align: center;
+    align-content: center;
 }
 
-#loginButton:hover{
+.ml-4:hover{
    color:#001a33;
    background-color: ghostwhite;
 }
