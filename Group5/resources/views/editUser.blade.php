b8efd64fc034f2422385354fd84c21c45680c57d
LanceJosh <lancejoshb@gmail.com>
2023-11-14 14:37:33 +0800

    admin user RUD functions working

--- Group5/resources/views/editUser.blade.php
+++ Group5/resources/views/editUser.blade.php
@@ -1,16 +1,30 @@
-<form action="{{ route('users.update', $user->id) }}" method="POST">
+<form action="{{ route('users.update', $user->id) }}" method="POST" class="needs-validation" novalidate>
     @csrf
     @method('PUT')
 
-    <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="{{ $user->name }}">
+    <div class="form-group row">
+        <label for="name" class="col-sm-2 col-form-label">Name</label>
+        <div class="col-sm-10">
+            <input type="text" name="name" id="name" value="{{ $user->name }}" class="form-control" required>
+            <div class="invalid-feedback">
+                Please enter a name.
+            </div>
+        </div>
     </div>
 
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" value="{{ $user->email }}">
+    <div class="form-group row">
+        <label for="email" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+            <input type="email" name="email" id="email" value="{{ $user->email }}" class="form-control" required>
+            <div class="invalid-feedback">
+                Please enter a valid email address.
+            </div>
+        </div>
     </div>
 
-    <button type="submit">Update</button>
+    <div class="form-group row">
+        <div class="col-sm-10 offset-sm-2">
+            <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+    </div>
 </form>
