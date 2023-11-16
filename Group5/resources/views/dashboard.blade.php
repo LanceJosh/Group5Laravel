51460400a0ddd1f4d1cac1156a0c23c903d69822
crsnnz6 <126161848+crsnnz6@users.noreply.github.com>
2023-11-16 16:23:30 +0800

    modified:   Group5/resources/views/dashboard.blade.php

--- Group5/resources/views/dashboard.blade.php
+++ Group5/resources/views/dashboard.blade.php
@@ -8,7 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <button><a href="{{route('job.create')}}">Post a Job</a></button>
                 <x-welcome />
             </div>
         </div>
