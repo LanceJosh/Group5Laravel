096bf27e40233b84f11e0cddf3e88742bafe1247
Sofhia Lacandola <biancaxsofhia@gmail.com>
2023-11-23 14:50:37 +0800

    css for create jobs

--- Group5/resources/views/jobs/create-job.blade.php
+++ Group5/resources/views/jobs/create-job.blade.php
@@ -1,43 +1,56 @@
-
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Employer Job Listings') }}
+        </h2>
+    </x-slot>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('style-create-job.css') }}">
     <title>Create Job</title>
 </head>
 
 <body>
-    <h1>Create a Job</h1>
     <form method="post" action="{{route('job.store')}}">
         @csrf
         @method('post')
-        <div>
-            <label>Job Title: </label>
-            <input type="text" name="job_title" placeholder="Job title" />
-        </div>
-        <div>
-            <label>Description: </label>
-            <input type="text" name="description" placeholder="Description" />
-        </div>
-        <div>
-            <label>Salary: $</label>
-            <input type="number" name="salary" placeholder="Salary" />
-        </div>
-        <div>
-            <label>Job Type: </label>
-            <div>
-                <label><input type="radio" name="is_fulltime" value="1"> Full Time</label>
+        <div class="background">
+            <img src="{{URL::asset('images/image.jpg')}}" width="1500px">
+            <div class="job-container">
+                <h2>Create a  Job</h2>
+                <div class="job-input">
+                    <label>Job Title: </label>
+                    <input type="text" name="job_title" placeholder="Job title" />
+                </div>
+                <div class="job-input">
+                    <label>Description: </label>
+                    <input type="text" name="description" placeholder="Description" />
+                </div>
+                <div class="job-input">
+                    <label>Salary: $</label>
+                    <input type="number" name="salary" placeholder="Salary" />
+                </div>
+                <div class="job-input">
+                    <label>Job Type: </label>
+                    <div>
+                        <label><input type="radio" name="is_fulltime" value="1"> Full Time</label>
+                    </div>
+                    <div>
+                        <label><input type="radio" name="is_fulltime" value="0"> Part Time</label>
+                    </div>
+                </div>
+                <div class="button">
+                    <input type="submit" value="ADD JOB"/>
+                </div>
             </div>
-            <div>
-                <label><input type="radio" name="is_fulltime" value="0"> Part Time</label>
-            </div>
-        </div>
-        <div>
-            <input type="submit" value="Add Job"/>
         </div>
     </form>
+    
 </body>
 
 </html>
+</x-app-layout>
