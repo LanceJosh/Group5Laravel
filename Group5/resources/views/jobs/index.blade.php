891faba2ad61ae619af9eb4ad000e1c15c213f9d
LanceJosh <lancejoshb@gmail.com>
2023-11-23 13:06:45 +0800

    simple design
    navbar updates jobs, about us, contact

--- Group5/resources/views/jobs/index.blade.php
+++ Group5/resources/views/jobs/index.blade.php
@@ -1,36 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Employer Job Listings') }}
+        </h2>
+    </x-slot>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jobs</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Jobs</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
 
-<body>
-    <h1>My Jobs</h1>
-    <div>
-        @if(session()->has('success'))
-        <h6>{{session('success')}}</h6>
-        @endif
+    <body>
+        <div class="container">
+            <h1 class="mt-5 mb-3">My Jobs</h1>
+            <div class="alert alert-success" role="alert">
+                @if(session()->has('success'))
+                {{session('success')}}
+                @endif
+            </div>
+            <a href="{{route('job.create')}}" class="btn btn-primary mb-3">Post a Job</a>
+            <div>
+                @foreach($jobs as $job)
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $job->job_title }}</h5>
+                        <p class="card-text">
+                            <strong>Description:</strong> {{ $job->description }} <br>
+                            <strong>Salary:</strong> ${{ $job->salary }} <br>
+                            <strong>Full Time:</strong> {{ $job->is_fulltime ? 'Yes' : 'No' }} <br>
+                            <strong>Employer:</strong> {{ $job->employer->name }}
+                        </p>
+                        <a href="{{route('job.edit', ['job' => $job])}}" class="btn btn-warning">Edit</a>
+                        <form method="post" action="{{route('job.delete', ['job' => $job])}}">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete" class="btn btn-danger" />
+                        </form>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </body>
+
+    </html>
+
+
     </div>
-    <a href="{{route('job.create')}}"><button onclick="{{route('job.create')}}">Post a Job</button></a>
-    <div>
-        @foreach($jobs as $job)
-        <p>
-            <strong>Job Title:</strong> {{ $job->job_title }} <br>
-            <strong>Description:</strong> {{ $job->description }} <br>
-            <strong>Salary:</strong> ${{ $job->salary }} <br>
-            <strong>Full Time:</strong> {{ $job->is_fulltime ? 'Yes' : 'No' }} <br>
-            <strong>Employer:</strong> {{ $job->employer->name }}
-            <a href="{{route('job.edit', ['job' => $job])}}">Edit</a>
-        <form method="post" action="{{route('job.delete', ['job' => $job])}}">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete" />
-        </form>
-        </p>
-        @endforeach
-    </div>
-</body>
-</html>
+
+</x-app-layout>
