4cd29ebe827b2ba5c2ec5daefb11e81269d7f8c9
charm2602 <118341084+charm2602@users.noreply.github.com>
2023-12-11 18:12:38 +0800

    my applications.php

--- Group5/resources/views/applicant/apply.blade.php
+++ Group5/resources/views/applicant/apply.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Jobs</title>
+        <title>Hire Lab&Co. | Jobs</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
 
--- Group5/resources/views/applicant/my-applications.blade.php
+++ Group5/resources/views/applicant/my-applications.blade.php
@@ -1,18 +1,68 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('My applications') }}
-        </h2>
-    </x-slot>
-    <!DOCTYPE html>
-    <html lang="en">
-
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Applications</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+
+ <title>Hire Lab&Co. | My Applications</title>
+ <meta name="description" content="Online Job Management / Job Portal" />
+	<meta name="keywords" content="job, work, resume, applicants, application, employee, employer, hire, hiring, human resource management, hr, online job management, company, worker, career, recruiting, recruitment" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="Hire Lab&Co." />
+    <meta property="og:description" content="Online Job Management / Job Portal" />
+    <link rel="shortcut icon" href="images/logo.png">
+
+	
+	<link rel="stylesheet" href="icons/linearicons/style.css">
+	<link rel="stylesheet" href="icons/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="icons/simple-line-icons/css/simple-line-icons.css">
+	<link rel="stylesheet" href="icons/ionicons/css/ionicons.css">
+	<link rel="stylesheet" href="icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="icons/rivolicons/style.css">
+	<link rel="stylesheet" href="icons/flaticon-line-icon-set/flaticon-line-icon-set.css">
+	<link rel="stylesheet" href="icons/flaticon-streamline-outline/flaticon-streamline-outline.css">
+	<link rel="stylesheet" href="icons/flaticon-thick-icons/flaticon-thick.css">
+	<link rel="stylesheet" href="icons/flaticon-ventures/flaticon-ventures.css">
+<link href="css/Login.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+
+    .header{
+        top: 40px;
+    }
+    .tbody{
+        background-color: white;
+
+    }
+  
+     .table-responsive .tbody:hover{
+        background-color: #82caff; 
+        cursor: pointer;
+     }
+
+     .myApplications{
+        font-size: 30px;
+    color: #004d99;
+    letter-spacing: 2px;
+    line-height: 0.2em;
+    font-weight: bolder;
+    font-style: sans-serif;
+     }
+    
+    </style>
+</head>
+
+<x-app-layout>
+<br><br>
+    <div class="header">
+       <center> <h2 class="myApplications">
+            {{ __('My Applications') }}
+        </h2></center>
+    </div>
 
     <body>
         <div class="py-12">
@@ -23,10 +73,10 @@
                 </div>
                 @endif
                 @if($my_applications->count() > 0)
-                <div class="table-responsive">
+                <center><div class="table-responsive">
                     <table class="min-w-full divide-y divide-gray-200 shadow-sm rounded-lg overflow-hidden">
                         <thead class="bg-blue-200">
-                            <tr>
+                            <center><tr>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Count</th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Submission ID</th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Job title</th>
@@ -34,9 +84,9 @@
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Submitted At</th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Resume</th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">Actions</th>
-                            </tr>
+                            </tr></center>
                         </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
+                        <tbody class="tbody">
                             @php
                             $i=1;
                             @endphp
@@ -52,13 +102,33 @@
                                 <td class="px-6 py-4 whitespace-nowrap">{{$application->created_at->diffForHumans()}}</td>
                                 <td class="px-6 py-4 whitespace-nowrap"><a href="{{ url($application->resume) }}" target="_blank">{{$resume_filename}}</a></td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <form method="post" action="{{route('applicant.cancel', ['application' => $application])}}">
-                                        @csrf
-                                        @method('delete')
-                                        <input type="submit" value="Cancel" class="btn btn-danger" />
-                                    </form>
+                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#cancelModal{{$application->id}}">
+                                    Cancel </button>
                                 </td>
                             </tr>
+                            <div class="modal fade" id="cancelModal{{$application->id}}" tabindex="-1" role="dialog" aria-labelledby="cancelModalLabel{{$application->id}}" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="cancelModalLabel{{$application->id}}">Confirm Cancellation</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Are you sure you want to cancel this application?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <form method="post" action="{{route('applicant.cancel', ['application' => $application])}}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <input type="submit" value="Cancel" id="confirmCancelButton" class="btn btn-danger" />
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                             @else
                             <div class="card">
@@ -70,12 +140,14 @@
                             @endif
                         </tbody>
                     </table>
-                </div>
+                </div><center>
             </div>
 
         </div>
     </body>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </html>
     </div>
 
