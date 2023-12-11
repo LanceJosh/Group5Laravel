cb71117eeb1a3ba4a3247becf0b9026c93db6b82
Sofhia Lacandola <biancaxsofhia@gmail.com>
2023-12-11 18:29:55 +0800

    updated about us

--- Group5/resources/views/aboutus.blade.php
+++ Group5/resources/views/aboutus.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+<!DOCTYPE html>
+    <html lang="en">
+<head>
+<title>Contact</title>
     <style>
         footer {
             position: fixed;
@@ -16,79 +20,77 @@
             margin: 0 auto;
             padding: 0 20px;
         }
+
+        nav {
+        background-color: #013220;
+        color: #fff;
+        text-align: center;
+        padding: 10px 0;
+        }
+
+        nav ul {
+        list-style-type: none;
+        padding: 0;
+        }
+
+        nav ul li {
+        display: inline;
+        margin-right: 20px;
+        }
+
+        nav a {
+        text-decoration: none;
+        color: #fff;
+        font-weight: bold;
+        text-transform: uppercase;
+        transition: color 0.3s;
+        }
+
+        nav a:hover {
+        color: #ff6f61;
+        }
+
+        .text-about {
+        margin-top: -450px;
+        margin-left: 10%;
+        color: #0F52BA;
+        font-weight: bold;
+        }
+        .text-vision {
+        margin-top: -400px;
+        margin-left: 10%;
+        color: #0F52BA;
+        font-weight: bold;
+        }
+        .text-mission {
+        margin-top: -400px;
+        margin-left: 10%;
+        color: #0F52BA;
+        font-weight: bold;
+        }
+
+        p {
+        margin-left: 10%;
+        color: #0F52BA;
+        margin-bottom: 10px;
+        }
+        .aboutus{
+            width: 2100px;
+        }
     </style>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-                <html>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#0099ff" fill-opacity="1" d="M0,96L48,112C96,128,192,160,288,192C384,224,480,256,576,229.3C672,203,768,117,864,90.7C960,64,1056,96,1152,106.7C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
-                </svg>
-
-                <head>
-                    <title>Contact</title>
-                    <style>
-                        nav {
-                            background-color: #013220;
-                            color: #fff;
-                            text-align: center;
-                            padding: 10px 0;
-                        }
-
-                        nav ul {
-                            list-style-type: none;
-                            padding: 0;
-                        }
-
-                        nav ul li {
-                            display: inline;
-                            margin-right: 20px;
-                        }
-
-                        nav a {
-                            text-decoration: none;
-                            color: #fff;
-                            font-weight: bold;
-                            text-transform: uppercase;
-                            transition: color 0.3s;
-                        }
-
-                        nav a:hover {
-                            color: #ff6f61;
-                        }
-
-                        h1 {
-                            text-align: center;
-                        }
-
-                        p {
-                            text-align: center;
-                            padding: 10px 0;
-                        }
-                    </style>
-                </head>
+</head>
 
                 <body>
-
-                    <h1>About HireLab&Co.</h1>
-                    <p>At HireLab&Co, we believe in the power of connections and the transformative impact of finding the right job. Our platform is more than just a job listing website; it's a dynamic hub where talent meets opportunity. We are dedicated to revolutionizing the way individuals discover and secure their dream careers while assisting companies in building their ideal teams. With a commitment to innovation, inclusivity, and user-centric design, we strive to be the go-to destination for both job seekers and employers.</p>
-                    <h1>Our Mission</h1>
-                    <p>Our mission at HireLab&Co is to empower individuals on their professional journey by providing a seamless and personalized job-seeking experience. We aim to connect talented individuals with diverse and rewarding opportunities, fostering growth and success for both candidates and employers. By leveraging cutting-edge technology and a deep understanding of the evolving job market, we aspire to be the catalyst for positive career transformations.</p>
-                    <h1>Our Vision</h1>
-                    <p>At HireLab&Co, we envision a world where every individual has the tools and support needed to unlock their full professional potential. Our vision is to create a global community where talent transcends boundaries, and opportunities abound for everyone. Through our commitment to transparency, innovation, and a human-centric approach, we strive to be the driving force behind a future where the perfect job match is just a click away.
-
-
-
-
-                        .</p>
-
+                    <img class="aboutus" src="{{ URL::to('/images/about-us.png') }}">
+                    <h1 class="text-mission">Our Mission</h1>
+                    <p>Our mission at HireLab&Co is to empower individuals on their professional journey <br>by providing a seamless and personalized job-seeking experience. We aim to <br>connect talented individuals with diverse and rewarding opportunities, fostering <br>growth and success for both candidates and employers. By leveraging cutting-edge technology <br>and a deep understanding of the evolving job market, we aspire to be the catalyst for positive career <br>transformations.</p>
+                    <h1 class="text-vision">Our Vision</h1>
+                    <p>At HireLab&Co, we envision a world where every individual has the tools and support <br>needed to unlock their full professional potential. Our vision is to create a global <br>community where talent transcends boundaries, and opportunities abound for everyone. <br>Through our commitment to transparency, innovation, and a human-centric approach, we <br>strive to be the driving force behind a future where the perfect job match is just a click away.</p>
+                    <h1 class="text-about">About HireLab&Co.</h1>
+                    <p>At HireLab&Co, we believe in the power of connections and the transformative impact of finding the <br>right job. Our platform is more than just a job listing website; it's a dynamic hub where talent meets<br> opportunity. We are dedicated to revolutionizing the way individuals discover and secure their<br> dream careers while assisting companies in building their ideal teams. With a commitment to <br>innovation, inclusivity, and user-centric design, we strive to be the go-to destination for both <br>job seekers and employers.</p>
                 </body>
 
-                </html>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#0099ff" fill-opacity="1" d="M0,96L48,133.3C96,171,192,245,288,245.3C384,245,480,171,576,160C672,149,768,203,864,229.3C960,256,1056,256,1152,240C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-                </svg>
+    </html>
                 <footer>
                     <div class="footer-content">
                         <p>© 2023 HireLab&Co. | All rights reserved.</p>
