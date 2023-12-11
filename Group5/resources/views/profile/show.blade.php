4788e2ab73828be151d7ec7f123b0eb20d7d7517
crsnnz6 <126161848+crsnnz6@users.noreply.github.com>
2023-12-11 16:16:34 +0800

    modified:   Group5/app/Http/Controllers/ApplicationController.php 	modified:   Group5/app/Http/Controllers/UserController.php 	modified:   Group5/app/Models/User.php 	new file:   Group5/public/profile_photos/1784931677945252_wind-sign.png 	modified:   Group5/resources/views/employer/applicants.blade.php 	modified:   Group5/resources/views/profile/show.blade.php 	modified:   Group5/routes/web.php

--- Group5/resources/views/profile/show.blade.php
+++ Group5/resources/views/profile/show.blade.php
@@ -7,26 +7,50 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+        <div class="mt-6">
+                <h2 class="text-lg font-semibold leading-6 text-gray-900">Profile Picture</h2>
+                <div class="mt-4 flex items-center">
+                    <img id="profilePreview" class="h-24 w-24 rounded-full object-cover" src="{{ asset(auth()->user()->profile_photo_path) }}" alt="{{ auth()->user()->name }}">
+                    <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data" class="ml-4">
+                        @csrf
+                        @method('PUT')
 
-                <x-section-border />
+                        <input type="file" name="profile_photo" accept="image/*" class="hidden" id="profile_photo" onchange="previewProfilePicture(this);">
+                        
+                        <!-- Change button using basic HTML with larger padding -->
+                        <button type="button" class="ml-2 p-2 bg-gray-200 hover:bg-gray-300 rounded-md cursor-pointer" onclick="document.getElementById('profile_photo').click();">
+                            Change
+                        </button>
+
+                        <!-- Save button using basic HTML with larger padding -->
+                        <button type="submit" class="ml-4 p-2 bg-blue-500 hover:bg-blue-700 text-white rounded-md">
+                            Save
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <!-- End Profile Picture Update Form -->
+
+            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+            @livewire('profile.update-profile-information-form')
+
+            <x-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.update-password-form')
+            </div>
 
-                <x-section-border />
+            <x-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.two-factor-authentication-form')
+            </div>
 
-                <x-section-border />
+            <x-section-border />
             @endif
 
             <div class="mt-10 sm:mt-0">
@@ -34,11 +58,11 @@
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-section-border />
+            <x-section-border />
 
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
-                </div>
+            <div class="mt-10 sm:mt-0">
+                @livewire('profile.delete-user-form')
+            </div>
             @endif
         </div>
     </div>
