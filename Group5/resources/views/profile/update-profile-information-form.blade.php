4beeed7e77711b3317c938bc7b815b5e3294b152
charm2602 <118341084+charm2602@users.noreply.github.com>
2023-12-13 08:39:26 +0800

    last

--- Group5/resources/views/profile/update-profile-information-form.blade.php
+++ Group5/resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="!photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full" style ="width: 150px; height: 150px;">
                 </div>
 
                 <!-- New Profile Photo Preview -->
--- Group5/resources/views/profile/view-profile.blade.php
+++ Group5/resources/views/profile/view-profile.blade.php
@@ -1,16 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
-</head>
+ <title>Hire Lab&Co. | Profile</title>
+ <meta name="description" content="Online Job Management / Job Portal" />
+	<meta name="keywords" content="job, work, resume, applicants, application, employee, employer, hire, hiring, human resource management, hr, online job management, company, worker, career, recruiting, recruitment" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="500" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="Hire Lab&Co." />
+    <meta property="og:description" content="Online Job Management / Job Portal" />
+    <link rel="shortcut icon" href="images/logo.png">
+
+	
+	<link rel="stylesheet" href="icons/linearicons/style.css">
+	<link rel="stylesheet" href="icons/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="icons/simple-line-icons/css/simple-line-icons.css">
+	<link rel="stylesheet" href="icons/ionicons/css/ionicons.css">
+	<link rel="stylesheet" href="icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="icons/rivolicons/style.css">
+	<link rel="stylesheet" href="icons/flaticon-line-icon-set/flaticon-line-icon-set.css">
+	<link rel="stylesheet" href="icons/flaticon-streamline-outline/flaticon-streamline-outline.css">
+	<link rel="stylesheet" href="icons/flaticon-thick-icons/flaticon-thick.css">
+	<link rel="stylesheet" href="icons/flaticon-ventures/flaticon-ventures.css">
+<link href="css/Login.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <body>
     <x-app-layout>
-    <div class="card mx-auto" style="max-width: 18rem;">
-        <img src="{{ asset($user->profile_photo_path) }}" class="card-img-top" alt="{{ $user->name }}">
+        <br><br><br>
+    <div class="card mx-auto" style="width: 35rem;">
+    <br>
+       <center> <img src="{{ asset($user->profile_photo_path) }}" class="card-img-top" alt="{{ $user->name }}"> </center>
         <div class="card-body">
             <h5 class="card-title">{{ $user->name }}</h5>
             <p class="card-text">{{ $user->role }}</p>
@@ -21,5 +50,28 @@
     </x-app-layout>
 
 </body>
+<style>
+    .card-title{
+    color:#001a33;
+    font-weight: bolder;
+    font-style: sans-serif;
+    font-size:  30px;
+    text-decoration: none;
 
+}
+.card-text{
+    font-weight: light;
+    font-style: sans-serif;
+    font-size:  20px;
+}  
+
+.card-img-top{
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+
+}
+
+
+</style>
 </html>
